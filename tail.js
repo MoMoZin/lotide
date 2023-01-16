5e5503b85a2e5440145d1bf26bd0f693ac3eccc0
kmomozin <khinmomozin@gmail.com>
2023-01-16 10:22:56 -0700

    refactored tailTest with C&M

--- tail.js
+++ tail.js
@@ -12,11 +12,5 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,14 @@
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 const tail = require('../tail.js');
 
-assertEqual(tail([1]), 5);
+
+describe('#tail', () => {
+  it('should get ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it('should get length of 3 to verify that tail did not change the actual array', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words); // no need to capture the return value since we are not checking it
+    assert.deepEqual(words.length, 3);
+  });
+});
